4e314119a9926baa8adc51ba8de7adde41f6de3c
nick.gong <nickgong90@gmail.com>
2019-04-24 18:03:54 +0800

    message

---
+++ TYBet188SDK.h
@@ -0,0 +1,14 @@
+//
+//  TYBet188SDK.h
+//  Pods
+//
+//  Created by ace on 2/22/19.
+//
+
+#ifndef TYBet188SDK_h
+#define TYBet188SDK_h
+
+#import "TY188Manager.h"
+#import "TY188ListManager+Favorates.h"
+
+#endif /* TYBet188SDK_h */
